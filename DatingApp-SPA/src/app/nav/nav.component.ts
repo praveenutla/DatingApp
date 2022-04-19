af7943e8ef783db51d29bbb7eaf681216568f3b6
kumarutl <kumarutl@PUN1NB101481>
2022-04-19 17:40:53 +0530

    Unauthirized added

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { AlertifyService } from '../_services/alertify.service';
 import { AuthService } from '../_services/auth.service';
 
@@ -11,7 +12,7 @@ export class NavComponent implements OnInit {
 
   model: any = {};
 
-  constructor(public auth: AuthService, private alertify: AlertifyService) { }
+  constructor(public auth: AuthService, private alertify: AlertifyService, private router:Router) { }
 
   ngOnInit() {
   }
@@ -22,9 +23,13 @@ export class NavComponent implements OnInit {
         this.alertify.success("login success");
       }, error => {
         this.alertify.error(error);
+      },
+      () => {
+        this.router.navigate(['/memberlists']);
       }
     )
   }
+
 
   loggedIn(){
     return this.auth.loggedIn();
@@ -33,6 +38,7 @@ export class NavComponent implements OnInit {
   logout(){
     localStorage.removeItem('token');
     this.alertify.message("logged out");
+    this.router.navigate(['/home']);
   }
 
 }
